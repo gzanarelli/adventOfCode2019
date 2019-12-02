22c41ee3e217d46446390d5be6a33b300d5e9a89
Zanarelli <gzanarelli@G.Zanarelli>
2019-12-02 23:01:12 +0100

    up day_02

--- day_02/02.js
+++ day_02/02.js
@@ -1,13 +1,58 @@
 
-const data = require('./data.json')
-
+const _data = require('./data.json')
 const finish = 19690720
 let x = []
 
-for (let k = 0; k < 100; k++) {
-  for (let j = 0; j < 100; j++) {
-    data.splice(1, 1, k)
-    data.splice(2, 1, j)
+const brut = () => {
+  var startTime = new Date().getTime()
+  var elapsedTime = 0
+  
+  for (let k = 0; k < 100; k++) {
+    for (let j = 0; j < 100; j++) {
+      const data = _data.slice(0)
+      data.splice(1, 1, k)
+      data.splice(2, 1, j)
+      for (let i = 0; i < data.length + 1; i++) {
+        if (i % 4 === 0 && i !== 0) {
+          let tt = 0
+          if (x[0] === 1) {
+            tt = data[x[1]] + data[x[2]]
+            data.splice([x[3]], 1, tt)
+            x = []
+          } else if (x[0] === 2) {
+            tt = data[x[1]] * data[x[2]]
+            data.splice(data[x[3]], 1, tt)
+            x = []
+          } else {
+            x = []
+            break
+          }
+        }
+        x.push(data[i])
+      }
+      if (data[0] === finish) {
+        elapsedTime = new Date().getTime() - startTime
+        console.log(elapsedTime)
+        return (100 * data[1] + data[2])
+      }
+    }
+  }
+}
+console.log(brut())
+
+const douce = (a, b) => {
+  var startTime = new Date().getTime()
+  var elapsedTime = 0
+  
+  // votre code à mesurer ici
+  
+
+  let data = _data.slice(0)
+  while (data[0] !== finish && (a < 100 || b < 100) ) {
+    data = _data.slice(0)
+    data.splice(1, 1, a)
+    data.splice(2, 1, b)
+
     for (let i = 0; i < data.length + 1; i++) {
       if (i % 4 === 0 && i !== 0) {
         let tt = 0
@@ -20,13 +65,23 @@ for (let k = 0; k < 100; k++) {
           data.splice(data[x[3]], 1, tt)
           x = []
         } else {
-          if (data[1] * 100 + data[2] === finish) {
-            console.log(data[1], data[2])
-          }
+          x = []
           break
         }
       }
       x.push(data[i])
     }
+    if (data[0] === finish) {
+      elapsedTime = new Date().getTime() - startTime
+      console.log(elapsedTime)
+      return (100 * data[1] + data[2])
+    } else {
+      if (a === 99) {
+        return douce(0, b + 1)
+      }
+      return douce(a + 1, b)
+    }
   }
 }
+
+console.log(douce(0, 0))
---
+++ script.sh
@@ -0,0 +1,12 @@
+for i in `seq 1 25`
+do
+	if [ $i -lt 10 ]
+		
+	then mkdir day_0$i
+		
+	else mkdir day_$i
+		
+	fi
+
+done
+
